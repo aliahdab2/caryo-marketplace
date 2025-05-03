117b74e924a771ab76273bb85940e1f4e40c546b
Ali Ahdab <ali.ahdab@telavox.com>
2025-05-03 21:18:56 +0200

    add tag for controller

--- backend/autotrader-backend/src/main/java/com/autotrader/autotraderbackend/controller/HealthController.java
+++ backend/autotrader-backend/src/main/java/com/autotrader/autotraderbackend/controller/HealthController.java
@@ -1,8 +1,8 @@
+package com.autotrader.autotraderbackend.controller;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-package com.autotrader.autotraderbackend.controller;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
