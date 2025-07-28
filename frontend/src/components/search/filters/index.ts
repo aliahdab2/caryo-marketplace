1afe642607035b595a167374246adc54c061f78a
Ali Ahdab <ali.ahdab@telavox.com>
2025-07-28 16:45:35 +0200

    refactor: remove PriceRangeFilter and YearRangeFilter components from search filters; update centralized exports accordingly

--- frontend/src/components/search/filters/index.ts
+++ frontend/src/components/search/filters/index.ts
@@ -1,7 +1,5 @@
 // Centralized exports for search filter components
 export { default as MakeModelFilter } from './MakeModelFilter';
-export { default as PriceRangeFilter } from './PriceRangeFilter';
-export { default as YearRangeFilter } from './YearRangeFilter';
 export { default as TransmissionFilter } from './TransmissionFilter';
 export { default as BodyStyleFilter } from './BodyStyleFilter';
 export { default as FuelTypeFilter } from './FuelTypeFilter';
