0e049681a0b37c12db44a25e9ca3a091b5618155
Ali Ahdab <ali.ahdab@telavox.com>
2025-08-01 12:07:12 +0200

    refactor: remove unnecessary conditional annotation from EmailServiceImpl for cleaner code

--- backend/autotrader-backend/src/main/java/com/autotrader/autotraderbackend/service/EmailServiceImpl.java
+++ backend/autotrader-backend/src/main/java/com/autotrader/autotraderbackend/service/EmailServiceImpl.java
@@ -27,7 +27,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@ConditionalOnProperty(name = "spring.mail.host")
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
