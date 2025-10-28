a6d1056d939aa007c7c43ebbacd5a8bd013f82ea
Ali Ahdab <aliahdab2@gmail.com>
2025-10-28 15:12:48 +0100

    🔧 Refactor Dealer Model: Remove Unused Imports
    - Removed unused imports from the Dealer model to clean up the codebase and improve maintainability.

--- backend/autotrader-backend/src/main/java/com/autotrader/autotraderbackend/model/Dealer.java
+++ backend/autotrader-backend/src/main/java/com/autotrader/autotraderbackend/model/Dealer.java
@@ -9,8 +9,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
